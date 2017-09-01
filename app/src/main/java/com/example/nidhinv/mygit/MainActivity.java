592c41137a5ef045ba843488f4783cb343ace52a
Nidhin PV <nidhin.v@sonimtech.com>
2017-09-01 19:00:29 +0530

    Added functionality

--- app/src/main/java/com/example/nidhinv/mygit/MainActivity.java
+++ app/src/main/java/com/example/nidhinv/mygit/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.nidhinv.mygit;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Add(View view) {
-
+        ((EditText)view).setText(R.string.ading_files);
     }
 
     public void Sub(View view) {
